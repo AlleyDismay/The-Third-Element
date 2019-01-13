658c0df4b6ff4e08bfe79615617c08e135630ab5
AlleyDismay <31709905+AlleyDismay@users.noreply.github.com>
2019-01-12 21:17:19 -0500

    Simpler save function to call.

--- Game/save.py
+++ Game/save.py
@@ -1,12 +1,13 @@
-def saveStats(earth, fire, water, health, money, items):
+def saveStats(self):
     f = open("save.dat", "w")
-    if earth: f.write("earth ")
-    if fire: f.write("fire ")
-    if water: f.write("water ")
+    gems = self.treasure.gems
+    if gems["earth"]: f.write("earth ")
+    if gems["fire"]: f.write("fire ")
+    if gems["water"]: f.write("water ")
     f.write("\n")
-    f.write(str(health)+"\n")
-    f.write(str(money)+"\n")
-    for i in items:
+    f.write(str(self.treasure.health)+"\n")
+    f.write(str(self.treasure.money)+"\n")
+    for i in self.treasure.collectedItems:
         f.write(i + " ")
     f.write("\n")
-    f.close() 
+    f.close()
